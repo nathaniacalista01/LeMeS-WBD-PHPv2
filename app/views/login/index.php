a8dcf5e568b9633831ade52bde114044abbb5151
Nathania Calista <13521139@std.stei.itb.ac.id>
2023-09-30 12:44:11 +0000

    Add api folder

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -8,6 +8,9 @@
         <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
     </head>
     <body>
+        <?php 
+           
+        ?>
         <div class="login">
             <div class="login-content">
                 <div class="login-img">
@@ -15,22 +18,22 @@
                 </div>
 
                 <div class="login-forms">
-                    <form action="" class="login-register" id="login-in">
+                    <form action="api/login.php" method="post" class="login-register" id="login-in">
                         <h1 class="login-title">Sign In</h1>
 
                         <div class="login-box">
                             <i class='bx bx-user login-icon'></i>
-                            <input type="text" placeholder="Username or Email" class="login-input">
+                            <input type="text" placeholder="Username or Email" name="username" class="login-input">
                         </div>
 
                         <div class="login-box">
                             <i class='bx bx-lock-alt login-icon'></i>
-                            <input type="password" placeholder="Password" class="login-input">
+                            <input type="password" placeholder="Password" name="password" class="login-input">
                         </div>
 
                         <a href="#" class="login-forgot">Forgot password ?</a>
 
-                        <a href="#" class="login-button">Sign In</a>
+                        <button type="submit" class="login-button">Sign In</button>
 
                         <div>
                             <span class="login-account">Don't have an Account ?</span>
