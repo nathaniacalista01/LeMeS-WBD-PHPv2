77a90f5683d6df687418948b92e67b7e73a2aec4
nathaniacalista01 <nathania.calista01@gmail.com>
2023-10-02 15:57:04 +0700

    feat : client side validation for username

--- api/auth/login.php
+++ api/auth/login.php
@@ -1,11 +1,22 @@
 <?php
-session_start();
+    require_once("../../app/core/App.php");
+    require_once("../../app/core/Database.php");
+    require_once("../../app/models/Student.php");
+    require_once("../../app/core/Table.php");
+    require_once("../../config/config.php");
+    require_once("../../app/models/Teacher.php");
+    $xml = file_get_contents('php://input');
+    $data = json_decode($xml, true);
+    $student = new Student();  
+    $teacher = new Teacher();
 
-if(isset($_POST["username"]) && isset($_POST["password"])){
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    // Login authentication user
-    
-}else{
-    echo "Gagal pass data";
-}?>
+    if(isset($data["username"])){
+        $username = $data["username"];
+        $result = $student->getStudentByUsername($username);
+        if($result == null){
+            echo json_encode(array("status" => "fail","message" => "Username doesn't exist"));
+        }else{
+            echo json_encode(array("status" => "success","message" => "Username exists"));
+        }
+    }
+?>
