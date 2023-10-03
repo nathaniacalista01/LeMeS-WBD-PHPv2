013ffe3a61c43ab51ad8db60138403f930285640
nathaniacalista01 <nathania.calista01@gmail.com>
2023-10-03 23:38:25 +0700

    fix : sidebar button

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,9 +3,6 @@
         public function index(){
            header("Location: /course/lists/page=1");
         }
-        public function redirect(){
-            echo "redirect";
-            return $this->view('home','index',[]);
-        }
+        
     }
 ?>
--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,23 +1,12 @@
 <?php
     class LoginController extends Controller{
         public function index(){
-            return $this->view('login','index',[]);
-        }
-
-        public function redirect($params){
-            // Pisahkan message dengan kontennya
-            $message = explode("=",$params);
-            // Kalau pemecahan berhasil, decode
-            if(count($message) == 2){
-                $content = urldecode($message[1]);
-                return $this->view('login','index',["message" => $content]);
+            if(isset($_SESSION["user_id"])){
+                header("Location: /");
+                exit;
             }else{
                 return $this->view('login','index',[]);
             }
-        }
-
-        public function testing(){
-            print_r($_SESSION);
         }
     }
 ?>
