6d7263bfc99b7ae0d9013aa22e552fee5568478e
nathaniacalista01 <nathania.calista01@gmail.com>
2023-10-06 16:04:05 +0700

    reused pagination

--- app/views/admin/courses.php
+++ app/views/admin/courses.php
@@ -10,7 +10,7 @@
 
 <body>
     <?php include __DIR__ . '/../navbar/navbar.php'?>
-
+    
 	<section class="home-section">
 		<div class="main">
 			<div class="report-container">
@@ -73,43 +73,12 @@
                                 }
                             ?>
                             
-                        </table>
-                        
-                        <div class="paging">           
-                    <div class="pagination">
+                        </table>                        
                         <?php 
-                            $start_index = $data["page_number"];
-                            $max_page = $data["max_page"];
-                            $prev_index = $start_index <= 1 ? 1 : $start_index-1;
-                            if($start_index > 1){
-                                $prev_index = $start_index-1;
-                                echo "<a href='/admin/courses/page=$prev_index'>PREV</a>";
-                            }
-                            for($i = $prev_index; $i < $start_index+2;$i++){
-                                if($i == $max_page){
-                                    break;
-                                }
-                                if($i == $start_index){
-                                    echo "<a class='pagination-active' style='background-color:#9e51d8;color:white;' id='pagination-active'>$i</a>";
-                                }else{
-                                    echo "<a href='/admin/courses/page=$i' id='pagination-number'>$i</a>";
-                                }
-                            }
-                            if($start_index < $max_page-2){
-                                echo "<a>...</a>";
-                            }
-                            if($start_index == $max_page){
-                                echo "<a style='background-color:#9e51d8;color:white;' href='/admin/users/page=$max_page'>$max_page</a>";
-                            }else{
-                                echo "<a href='/admin/courses/page=$max_page'>$max_page</a>";
-                            }
-                            if($start_index < $max_page){
-                                $next_index = $start_index + 1;
-                                echo "<a href='/admin/courses/page=$next_index'> &nbsp;  NEXT</a>";
-                            }
+                            $href = "courses";
+                            include __DIR__ . '/../components/pagination.php'
                         ?>
-                    </div>
-			    </div>
+
 			    </div>
 			</div>
 		</div>
--- app/views/admin/users.php
+++ app/views/admin/users.php
@@ -96,41 +96,10 @@
                                 ?>
                             </tbody>
                         </table>
-                <div class="paging">           
-                    <div class="pagination">
                         <?php 
-                            $start_index = $data["page_number"];
-                            $max_page = $data["max_page"];
-                            $prev_index = $start_index <= 1 ? 1 : $start_index-1;
-                            if($start_index > 1){
-                                $prev_index = $start_index-1;
-                                echo "<a href='/admin/users/page=$prev_index'>PREV</a>";
-                            }
-                            for($i = $prev_index; $i < $start_index+2;$i++){
-                                if($i == $max_page){
-                                    break;
-                                }
-                                if($i == $start_index){
-                                    echo "<a class='pagination-active' style='background-color:#9e51d8;color:white;' id='pagination-active'>$i</a>";
-                                }else{
-                                    echo "<a href='/admin/users/page=$i' id='pagination-number'>$i</a>";
-                                }
-                            }
-                            if($start_index < $max_page-2){
-                                echo "<a>...</a>";
-                            }
-                            if($start_index == $max_page){
-                                echo "<a style='background-color:#9e51d8;color:white;' href='/admin/users/page=$max_page'>$max_page</a>";
-                            }else{
-                                echo "<a href='/admin/users/page=$max_page'>$max_page</a>";
-                            }
-                            if($start_index < $max_page){
-                                $next_index = $start_index + 1;
-                                echo "<a href='/admin/users/page=$next_index'> &nbsp;  NEXT</a>";
-                            }
+                            $href = "users";
+                            include __DIR__ . '/../components/pagination.php'
                         ?>
-                    </div>
-			    </div>
 		    </div>
         </section>
     </body>
